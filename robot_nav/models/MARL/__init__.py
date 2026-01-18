5a7b1fc3a5b74d168dd47e40039a4a89ee201e65
SIYU LI <sli230@uic.edu>
2026-01-18 14:11:10 -0600

    Update __init__.py

--- robot_nav/models/MARL/__init__.py
+++ robot_nav/models/MARL/__init__.py
@@ -1,5 +1,17 @@
 """MARL Models Module - Multi-Agent RL Models for Navigation."""
 
-from robot_nav.models.MARL.lidar_encoder import LiDAREncoder, LiDAREncoderMLP, normalize_lidar_scan
+from robot_nav.models.MARL.lidar_encoder import (
+    LiDARSectorEncoder,
+    LiDAREncoderMLP,
+    LiDAREncoderCNN,
+    create_lidar_encoder,
+    normalize_lidar_scan,
+)
 
-__all__ = ["LiDAREncoder", "LiDAREncoderMLP", "normalize_lidar_scan"]
+__all__ = [
+    "LiDARSectorEncoder",
+    "LiDAREncoderMLP",
+    "LiDAREncoderCNN",
+    "create_lidar_encoder",
+    "normalize_lidar_scan",
+]
